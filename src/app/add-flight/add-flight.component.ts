52555b85d89ea0fe797dc7b1a389234de4c91d25
Brett Pasula <brettpasula@gmail.com>
2024-02-05 17:21:04 -0800

    WIP implementing add flight

--- src/app/add-flight/add-flight.component.ts
+++ src/app/add-flight/add-flight.component.ts
@@ -1,18 +1,65 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { FlightService } from 'src/services/flight-service.service';
 
 @Component({
   selector: 'add-flight',
   templateUrl: './add-flight.component.html',
-  styleUrls: ['./add-flight.component.css']
+  styleUrls: ['./add-flight.component.css'],
 })
 export class AddFlightComponent {
-  public form: FormGroup = new FormGroup({
-    flightNumberPrefix: new FormControl('', Validators.maxLength(2)),
-    flightNumber: new FormControl('', Validators.maxLength(4)),
-    from: new FormControl('', Validators.maxLength(3)),
-    to: new FormControl('', Validators.maxLength(3)),
-    departureDate: new FormControl(''),
-    aircraftRegistration: new FormControl('')
-  });
+  private _flightService: FlightService;
+  private _router: Router;
+  form: FormGroup;
+  flightNumberPrefix: FormControl;
+  flightNumber: FormControl<Number | null>;
+  fromAirport: FormControl<string | null>;
+  toAirport: FormControl<string | null>;
+  departureDate: FormControl<Date | null>;
+  aircraftRegistration: FormControl<string | null>;
+
+  constructor() {
+    this._flightService = inject(FlightService);
+    this._router = inject(Router);
+
+    this.flightNumberPrefix = new FormControl('', [
+      Validators.required,
+      Validators.maxLength(2),
+    ]);
+    this.flightNumber = new FormControl(null, [
+      Validators.required,
+      Validators.maxLength(4),
+    ]);
+    this.fromAirport = new FormControl(null, [
+      Validators.required,
+      Validators.maxLength(3),
+    ]);
+    this.toAirport = new FormControl(null, [
+      Validators.required,
+      Validators.maxLength(3),
+    ]);
+    this.departureDate = new FormControl(null, [Validators.required]);
+    this.aircraftRegistration = new FormControl(null, [Validators.required]);
+
+    this.form = new FormGroup([
+      this.flightNumberPrefix,
+      this.flightNumber,
+      this.fromAirport,
+      this.toAirport,
+      this.departureDate,
+      this.aircraftRegistration,
+    ]);
+  }
+
+  onSubmit() {
+    this._flightService.addFlight(this.form.value).subscribe({
+      error: (error) => console.error(error),
+      complete: () => this._router.navigate(['/']),
+    });
+  }
 }
